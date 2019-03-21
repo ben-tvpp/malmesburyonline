ae04de52c20a073a4220cdebdef06b6c0bf0372c
Ben Edwards <Ben Edwards>
2019-03-21 15:39:58 +0000

    Make seed file

--- malmesburyonline/db/seeds.rb
+++ malmesburyonline/db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Walk.create!([
+  {name: "From Court House to St John's", description: "This is the description of  the walk", date_from: nil, date_to: nil},
+  {name: "Market Cross To Abby", description: "Go to the Market Cross to the Abby", date_from: nil, date_to: nil}
+])
+WalkSection.create!([
+  {image: "IMG_8844.JPG", name: "IMG_8844.JPG", walk_id: 1},
+  {image: "IMG_8845.JPG", name: "IMG_8845.jpg", walk_id: 1},
+  {image: "IMG_8846.JPG", name: "IMG_8846.jpg", walk_id: 1},
+  {image: "IMG_8847.JPG", name: "IMG_8847.JPG", walk_id: 1},
+  {image: "IMG_8848.JPG", name: "IMG_8848.JPG", walk_id: 1}
+])
+WalkSectionButton.create!([
+  {walk_section_id: 1, next_walk_section_id: 2, button_text: "Left down the alley", button_type: "B", button_vert_pos: "B", button_hor_pos: "C", from: nil, to: nil},
+  {walk_section_id: 2, next_walk_section_id: 4, button_text: "To the garage an on for a mile", button_type: "B", button_vert_pos: "B", button_hor_pos: "C", from: nil, to: nil},
+  {walk_section_id: 4, next_walk_section_id: 5, button_text: "Round the corner<br>past post office", button_type: "B", button_vert_pos: "B", button_hor_pos: "L", from: nil, to: nil},
+  {walk_section_id: 4, next_walk_section_id: 6, button_text: "Alternative Route", button_type: "B", button_vert_pos: "B", button_hor_pos: "R", from: nil, to: nil}
+])
